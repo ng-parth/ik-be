06a9db977b71f67fc89130aaf4dc3093bcb0e6dc
Parth <parth@halalbox.in>
2022-08-25 22:24:22 +0530

    Added sign generation logic.

--- api/getIkSign.js
+++ api/getIkSign.js
@@ -1,10 +1,18 @@
+import ImageKit from "imagekit";
 
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+export default function getIkSignature(request, response) {
+  const {IMK_URL, IMK_PBL_KEY, IMK_PVT_KEY} = process.env;
+  console.log('url: ', IMK_URL);
+  console.log('pvtKey: ', IMK_PBL_KEY);
+  console.log('pblKey: ', IMK_PVT_KEY);
+
+  const imagekit = new ImageKit({
+    publicKey : IMK_PBL_KEY,
+    privateKey : IMK_PVT_KEY,
+    urlEndpoint : IMK_URL,
   });
-
+  const authenticationParameters = imagekit.getAuthenticationParameters();
+  console.log(authenticationParameters);
+  response.status(200).json(authenticationParameters);
 }
 
